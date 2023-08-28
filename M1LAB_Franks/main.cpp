5ca4c93c02ca790ee7a9f939a5b64c1ea5fd9e20
frankshftcc <franksh6309@student.faytechcc.edu>
2023-08-28 11:36:23 -0400

    Added variables, added todo

--- M1LAB_Franks/main.cpp
+++ M1LAB_Franks/main.cpp
@@ -14,5 +14,22 @@ int main()
 {
     string name = "Franks";
     cout << "Welcome to the " << name << " orchard." << endl;
+
+    // declare our variables
+    int apples = 100;
+    double price_each = 0.25;
+
+    // print number of apples, price each
+    cout << "We have " << apples << " for sale." << endl;
+    cout << "Cost is $" << price_each << " each." << endl;
+
+    double total_price = apples * price_each;
+    // then print the total price
+    cout << "The price of all 100 apples is $" << total_price << "." << endl;
+
+    // then print the total price
+    // TODO: ask the user how many to buy
+
+    // TODO: Make the prices look "correct" (ex. $25.00)
     return 0;
 }
