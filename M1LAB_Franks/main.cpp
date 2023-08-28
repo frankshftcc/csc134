20be5d247ce5018308985af3d6ac608f88414cf7
frankshftcc <franksh6309@student.faytechcc.edu>
2023-08-28 11:25:56 -0400

    Started M1Lab

---
+++ M1LAB_Franks/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+CSC 134
+M1LAB - Apple Orchard
+franks
+8/28
+Simple interactive store.
+*/
+
+int main()
+{
+    string name = "Franks";
+    cout << "Welcome to the " << name << " orchard." << endl;
+    return 0;
+}
