9618bbd9e30400da0b1f211a016a37eba66065d1
frankshftcc <franksh6309@student.faytechcc.edu>
2023-09-11 11:18:40 -0400

    M2T2 Finished

--- M2T2_franks/M2T2_franks/main.cpp
+++ M2T2_franks/M2T2_franks/main.cpp
@@ -7,14 +7,13 @@ using namespace std;
 CSC 134
 M2T2 - Receipt Calculator
 franksh
-9/6
+9/11
 This program should take the price of a meal, which is user entered.
 
 The user should enter:
 - The name of the food item
 - Its price
 - How many they want
-
 
 It should include:
   - The price before tax
@@ -24,18 +23,40 @@ It should include:
 
 int main()
 {
-  //declare our variables:
-  cout << "Receipt Calculator" << endl;
-  //TODO: ask the user the meal price
-  double menu_price = 5.99;
-  double tax_percent = 0.07; // for cumberland county
+  // Set up variables
+  double menu_price = 0; // set to 0 until user gives input
+  double tax_percent = 0.07; // standard tax rate (for Cumberland county)
+  double item_price; // price of item
+  int num_items; // number they want
+  string item_name; // name of what they want
+
+  // 1 - Greet the user
+  cout << "Welcome to the CSC 134 Grill." << endl;
+
+  // 2 - Ask the user the meal price
+  cout << "What would you like today?" << endl;
+  cin >> item_name;
+
+  cout << "What's the price on that item?" << endl;
+  cin >> item_price;
+
+  cout << "How many " << item_name << " do you want? " << endl;
+  cin >> num_items;
+
+  // 3 - Calculate the menu price
+  menu_price = num_items * item_price;
+
+  // 4 - Print the receipt
+
   double tax_dollars = menu_price * tax_percent; // 7% of meal price
   double total_price = menu_price + tax_dollars; //price including tax
-  // calculate the tax amount and the total
-  // we #include <iomanip> at the top and use this magic below
-  // to use 2 decimal places
+
+  // Calculate the tax amount and the total
+  // We #include <iomanip> at the top and use this magic below
+  // To round 2 decimal places; ex. 2.04555 -> 2.05
   cout << fixed << setprecision(2);
-  // print output
+  // Print output
+  cout << "Order: " << num_items << " " << item_name << "(s)" << endl;
   cout << "Menu price: $" << menu_price << endl;
   cout << "Tax:        $" << tax_dollars << endl;
   cout << "_________________________" << endl;
