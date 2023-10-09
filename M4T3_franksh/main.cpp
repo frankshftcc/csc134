3b8e9d363dbafc1e3bf68a6c8c855808cd1401ec
frankshftcc <franksh6309@student.faytechcc.edu>
2023-10-09 11:16:03 -0400

    M4T3 completed
    hurray

--- M4T3_franksh/main.cpp
+++ M4T3_franksh/main.cpp
@@ -19,6 +19,7 @@ int main()
     const int NUM_DAYS = 5;
     int todays_cars;
     int total_cars = 0;
+    double average;
     cout << "Part 1: Counting Loop with for" << endl;
     cout << "How many cars did you see each day?" << endl;
     for (int i=0; i < NUM_DAYS; i++){
@@ -29,17 +30,50 @@ int main()
 
     cout << "Total cars seen was: " << total_cars;
     cout << " over " << NUM_DAYS << " days" << endl;
+    // find average as a double
+    average = (double) total_cars/NUM_DAYS;
+    cout << "Average per day = " << average << endl;
     cout << endl;
+
+
     cout << "Part 2: Count with while" << endl;
     //TODO ask the user how many cars they saw
     // each day then add them up
     int i = 0;
+    total_cars = 0;
     while (i < NUM_DAYS) {
-        cout << "Day# " << i << endl;
-        i++;
-
+        cout << "Day# " << i << ": ";
+        cin >> todays_cars;
+        total_cars += todays_cars; // add today to total
+        i++; // go to next day
     }
 
+    cout << "Total cars seen was: " << total_cars;
+    cout << " over " << NUM_DAYS << " days" << endl;
+    // find average as a double
+    average = (double) total_cars/NUM_DAYS;
+    cout << "Average per day = " << average << endl;
+    cout << endl;
 
+
+    // Part 3 - Sentinel
+    cout << "Enter how many cars you saw per day." << endl;
+    cout << "or -1 to finish." << endl;
+    bool keep_going = true;
+    int day = 0;
+    total_cars = 0;
+    while (keep_going) {
+        cout << "Day# " << day << ": ";
+        cin >> todays_cars;
+        if (todays_cars == -1) {
+            cout << "OK, Done" << endl;
+            keep_going = false;
+        }
+        else {
+            total_cars += todays_cars;
+            day++;
+        }
+    }
+    cout << "Total = " << total_cars << " cars" << endl;
     return 0;
 }
